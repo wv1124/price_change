25a343f2473f635be922bdfe544001729d8d64d1
Wang Wei <wangwei1780@qianmi.com>
2015-08-22 03:25:04 +0800

    [EPos]删除不用文件
    Commiter: Wang Wei

--- app/src/main/java/com/qianmi/hack/network/NetworkRequest.java
+++ app/src/main/java/com/qianmi/hack/network/NetworkRequest.java
@@ -23,29 +23,4 @@ public class NetworkRequest {
 
         return mServerAgency;
     }
-
-    public void loginRequest(String username, String password) {
-        LoginRequest loginRequest = new LoginRequest();
-        loginRequest.username = "caozupeng";
-        loginRequest.password = "caozupeng";
-
-        GsonRequest mRequest = new GsonRequest(
-                PcApplication.SERVER_URL + "api-token-auth/", loginRequest, Token.class,
-                new Response.Listener<Token>() {
-                    @Override
-                    public void onResponse(Token resp) {
-                        L.d("TAG", "token is " + resp.token);
-                        PcApplication.TOKEN = resp.token;
-
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("TAG", error.getMessage(), error);
-            }
-        });
-        ServerConnector.getInstance(PcApplication.getInstance()).add(mRequest);
-    }
-
-
 }
