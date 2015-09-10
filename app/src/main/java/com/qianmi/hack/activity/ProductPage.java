1207f43e923bf4c61bc47bf1bf0cb91f075de69c
曹祖鹏 <chinalibra@163.com>
2015-09-10 20:50:59 +0800

    增加产品搜索bar

--- app/src/main/java/com/qianmi/hack/activity/ProductPage.java
+++ app/src/main/java/com/qianmi/hack/activity/ProductPage.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +14,8 @@ import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -30,6 +34,8 @@ import com.qianmi.hack.bean.ProductListResult;
 import com.qianmi.hack.network.GsonRequest;
 import com.qianmi.hack.utils.L;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -59,6 +65,11 @@ public class ProductPage extends Fragment implements OnRefreshListener, View.OnC
     private LinearLayout loading;
     //下拉刷新组件
     private PullToRefreshLayout mPullToRefreshLayout;
+    //搜索组件
+    private EditText etSearch;
+    private String mKeyword;
+    private ImageView deleteText;
+    private Button btnSearch;
 
 
     private Handler mHandler = new Handler() {
@@ -83,6 +94,54 @@ public class ProductPage extends Fragment implements OnRefreshListener, View.OnC
         loading = (LinearLayout) view.findViewById(R.id.loading);
         loading.setVisibility(View.GONE);
         mListView = (ListView) view.findViewById(R.id.mListView);
+        initPullRefresh(view);
+        initView();
+        deleteText = (ImageView) view.findViewById(R.id.ivDeleteText);
+        deleteText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                etSearch.setText("");
+                btnSearch.callOnClick();
+            }
+        });
+        btnSearch = (Button) view.findViewById(R.id.btnSearch);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                currentPage = 1;
+                mKeyword = etSearch.getText().toString();
+                mList.clear();
+                Log.d(TAG, "keyword = "+ mKeyword);
+                requestDate(currentPage, mKeyword);
+            }
+        });
+        etSearch = (EditText) view.findViewById(R.id.etSearch);
+        etSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editable.length() == 0) {
+                    deleteText.setVisibility(View.GONE);
+                } else {
+                    deleteText.setVisibility(View.VISIBLE);
+                }
+
+            }
+        });
+        requestDate(currentPage, mKeyword);
+        return view;
+    }
+
+    private void initPullRefresh(View view) {
         mPullToRefreshLayout = (PullToRefreshLayout) view.findViewById(R.id.ptr_layout);
         // Now setup the PullToRefreshLayout
         ActionBarPullToRefresh.from(getActivity())
@@ -92,9 +151,6 @@ public class ProductPage extends Fragment implements OnRefreshListener, View.OnC
                 .listener(this)
                         // Finally commit the setup to our PullToRefreshLayout
                 .setup(mPullToRefreshLayout);
-        initView();
-        requestDate(currentPage);
-        return view;
     }
 
     @Override
@@ -103,7 +159,7 @@ public class ProductPage extends Fragment implements OnRefreshListener, View.OnC
         loading.setVisibility(View.VISIBLE);
         mList.clear();
         currentPage = 1;
-        requestDate(currentPage);
+        requestDate(currentPage, mKeyword);
         mPullToRefreshLayout.setRefreshComplete();
     }
 
@@ -159,17 +215,32 @@ public class ProductPage extends Fragment implements OnRefreshListener, View.OnC
         };
     }
 
-    private void requestDate(int currentPage) {
+    private void requestDate(int currentPage, String keyword) {
         GsonRequest.Builder<ProductListResult> builder = new GsonRequest.Builder<>();
-        GsonRequest request = builder.retClazz(ProductListResult.class)
-                .setUrl(String.format("%s/supproducts/?page=%d", PcApplication.SERVER_URL, currentPage))
-                .registerResListener(createSuccessListener())
-                .registerErrorListener(((BaseActivity) getActivity()).createErrorListener())
-                .method(Request.Method.GET)
-                .create();
+        StringBuilder url = new StringBuilder();
+
+        url.append(PcApplication.SERVER_URL).append("supproducts/?page=").append(currentPage);
+
+        if (keyword != null && !"".equals(keyword)) {
+            try {
+                String query = URLEncoder.encode(keyword, "utf-8");
+                url.append("&name=").append(query);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+        }
+        GsonRequest request = request = builder.retClazz(ProductListResult.class)
+                    .setUrl(url.toString())
+                    .registerResListener(createSuccessListener())
+                    .registerErrorListener(((BaseActivity) getActivity()).createErrorListener())
+                    .method(Request.Method.GET)
+                    .create();
+
         L.d("**************load data :currentPage=" + currentPage);
         MyVolley.getRequestQueue().add(request);
     }
+
 
     @Override
     public void onScrollStateChanged(AbsListView view, int scrollState) {
@@ -183,7 +254,7 @@ public class ProductPage extends Fragment implements OnRefreshListener, View.OnC
                 if (hasNext) {
                     Log.i(TAG, "loading... page: " + hasNext);
                     loading.setVisibility(View.VISIBLE);
-                    requestDate(currentPage);
+                    requestDate(currentPage, mKeyword);
                 }
             }
         }
@@ -260,17 +331,17 @@ public class ProductPage extends Fragment implements OnRefreshListener, View.OnC
         }
     }
 
-    private static class ViewHolder{
+    private static class ViewHolder {
         private ImageView mImage;
         private TextView name;
         private TextView salePrice;
         private TextView updateTime;
 
         public ViewHolder(View v) {
-            mImage = (ImageView)v.findViewById(R.id.img);
-            name = (TextView)v.findViewById(R.id.name);
-            salePrice = (TextView)v.findViewById(R.id.sale_price);
-            updateTime = (TextView)v.findViewById(R.id.update_time);
+            mImage = (ImageView) v.findViewById(R.id.img);
+            name = (TextView) v.findViewById(R.id.name);
+            salePrice = (TextView) v.findViewById(R.id.sale_price);
+            updateTime = (TextView) v.findViewById(R.id.update_time);
         }
     }
 
